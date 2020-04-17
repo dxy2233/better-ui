e48cd85f9e2989c59358656ec6576881d413d61b
dxy2233 <691369338@qq.com>
2020-04-17 17:30:46 +0800

    test provide

--- src/App.vue
+++ src/App.vue
@@ -1,104 +1,51 @@
 <template>
   <div id="app">
-    <baseTable :tableData="tableData" :rowClass="tableRowClassName">
-      <baseCol prop="projectCode" label="项目编号" />
-    </baseTable>
-    <baseCascader
-      v-model="dutyDepartment"
-      :data="dutyDepartmentData"
-      placeholder="请输入责任部门"
-    />
+    <baseForm ref="testForm" :form="form" :rules="rules">
+      <baseFormItem label="报告类型" prop="type" required>
+        <select v-model="form.type">
+          <option :value="1">基线检查报告</option>
+          <option :value="2">渗透测试报告</option>
+          <option :value="3">漏洞扫描报告</option>
+        </select>
+      </baseFormItem>
+      <button type="button" @click="submit">
+        <svg-icon icon-class="save" />保存
+      </button>
+    </baseForm>
   </div>
 </template>
 
 <script>
-import baseTable from './components/table'
-import baseCol from './components/col'
-import baseCascader from './components/cascader'
+import baseForm from './components/form-f'
+import baseFormItem from './components/formItem'
 
 export default {
   name: 'app',
-  components: { baseTable, baseCol, baseCascader },
+  components: { baseForm, baseFormItem },
   data() {
     return {
-      tableData: [{ projectCode: 123 }, { projectCode: 121 }],
-      dutyDepartment: '1EAE1A18B25344599A5351A606364AB4',
-      dutyDepartmentData: []
+      form: {
+        type: null
+      },
+      rules: {}
     }
   },
-  created() {
-    setTimeout(() => {
-      this.dutyDepartmentData = [
-        {
-          id: '1EAE1A18B25344599A5351A606364AB4',
-          orgName: '大数据智能中心',
-          orgCode: 'CQYD_005',
-          orgType: '业务扩展部门',
-          parentId: '9692EF34328747E59798D95BC1079344',
-          editVisble: false,
-          createVisble: false,
-          deleteVisble: false,
-          nextNodes: null
-        },
-        {
-          id: '22F7CA13747445B68BA459AAAB2D6AD7',
-          orgName: '全业务支撑中心',
-          orgCode: 'CQYD_004',
-          orgType: '业务支撑部门',
-          parentId: '9692EF34328747E59798D95BC1079344',
-          editVisble: false,
-          createVisble: false,
-          deleteVisble: false,
-          nextNodes: null
-        },
-        {
-          id: '616072E0F3FF4637AF0582FD8146A813',
-          orgName: '信息安全部',
-          orgCode: 'CQYD_001',
-          orgType: '信息安全部',
-          parentId: '9692EF34328747E59798D95BC1079344',
-          editVisble: false,
-          createVisble: false,
-          deleteVisble: false,
-          nextNodes: null
-        },
-        {
-          id: '7D9BA4FCA3554D79A38F5788EB1D600C',
-          orgName: '信息系统支撑部',
-          orgCode: 'CQYD_002',
-          orgType: '信息系统支撑部',
-          parentId: '9692EF34328747E59798D95BC1079344',
-          editVisble: false,
-          createVisble: false,
-          deleteVisble: false,
-          nextNodes: null
-        },
-        {
-          id: 'D29FC5BB59ED499A90D5518E46DB8671',
-          orgName: '计划部',
-          orgCode: 'CQYD_003',
-          orgType: '计划部',
-          parentId: '9692EF34328747E59798D95BC1079344',
-          editVisble: false,
-          createVisble: false,
-          deleteVisble: false,
-          nextNodes: null
-        }
-      ]
-    }, 1000)
-  },
-  methods: {
-    tableRowClassName(row) {
-      if (row.projectCode === 123) return 'prompt'
-    }
-  }
+  created() {},
+  methods: {}
 }
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+html,
+body {
+  margin: 0;
+  padding: 0;
+}
 #app {
-  /deep/ .prompt {
-    background: red;
+  min-height: 100vh;
+  background: #001f3f;
+  form {
+    background: #fff;
   }
 }
 </style>
---
+++ src/components/form-f.vue
@@ -0,0 +1,81 @@
+<script>
+export default {
+  name: 'Form',
+  props: {
+    form: {
+      type: Object
+    },
+    rules: {
+      type: Object
+    }
+  },
+  data() {
+    return {}
+  },
+  created() {},
+  methods: {},
+  render() {
+    return <form>sdfdsf</form>
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+form {
+  width: 80%;
+  margin: 0 auto;
+  .form-item {
+    display: flex;
+    align-items: center;
+    padding: 5px 0;
+    > label {
+      flex: 2;
+      align-self: flex-start;
+      text-align: right;
+      margin-top: 6px;
+      margin-right: 10px;
+      em {
+        color: red;
+        margin-right: 5px;
+      }
+    }
+    > div {
+      flex: 7;
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: flex-start;
+      align-items: center;
+      min-height: 31px;
+      label {
+        margin-right: 10px;
+        margin-top: 5px;
+      }
+      button {
+        padding: 0 10px;
+        margin: 0 10px 0 0;
+        background: #fff;
+        border: 1px solid #158ae7;
+        color: #158ae7;
+        &:hover {
+          background: #158ae7;
+          color: #fff;
+        }
+      }
+    }
+  }
+  .form-err {
+    color: #ff4949;
+    margin-left: 25%;
+  }
+  button {
+    display: block;
+    background: #045fc9;
+    color: #fff;
+    margin: 15px auto;
+    padding: 0 10px;
+    &:hover {
+      background: #158ae7;
+    }
+  }
+}
+</style>
